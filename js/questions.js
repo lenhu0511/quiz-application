74d6876c4eb1d7a3bffde2ff876c496ea8d4f327
lenhu0511 <lenhu0460@gmail.com>
2021-07-11 14:22:47 +0700

    A simple Quiz Application

---
+++ js/questions.js
@@ -0,0 +1,69 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is a MAJOR task of test planning?",
+    answer: "Scheduling test analysis and design tasks",
+    options: [
+      "Monitoring progress and test coverage",
+      "Initiating corrective actions",
+      "Measuring and analyzing results",
+      "Scheduling test analysis and design tasks"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following defines the expected results of a test?",
+    answer: "Test case specification",
+    options: [
+      "Test procedure specification",
+      "Test design specification",
+      "Test case specification",
+      "Test results"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which option best describes objectives for test levels with a life cycle model?",
+    answer: "Each level has objectives specific to that level",
+    options: [
+      "Each level has objectives specific to that level",
+      "The objectives of a test level don't need to be defined in advance",
+      "Objectives are the same for each test level",
+      "Objectives should be generic for any test level"
+    ]
+  },
+    {
+    numb: 4,
+    question: "One of the following is not a part of white box testing.",
+    answer: "Random testing",
+    options: [
+      "Random testing",
+      "Boundary value analysis",
+      "Data Flow testing",
+      "Statement testing"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is a test type?",
+    answer: "Functional testing",
+    options: [
+      "System testing",
+      "Functional testing",
+      "Component testing",
+      "Acceptance testing"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is failure?",
+    answer: "Deviation from expected result to actual result",
+    options: [
+      "Error in the program code",
+      "Fault in the system",
+      "Deviation from expected result to actual result",
+      "Defect in the software"
+    ]
+  },
+];
